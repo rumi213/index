b2cd3dd732d65effa1a77dd561a9ddd2cf35dc60
Mohammad Rumaisa <147631246+rumi213@users.noreply.github.com>
2024-04-29 19:21:22 +0530

    Add files via upload

---
+++ .js
@@ -0,0 +1,8 @@
+let navbar=document.querySelectorAll('header .navbar');
+document.queryselector('menu-btn').onclick=() =>{
+    navbar.classlist.add('active');
+}
+document.querySelectorAll('header .navbar');
+document.queryselector('#close-bar').onclick=() =>{
+    navbar.classlist.remove('active');
+}
